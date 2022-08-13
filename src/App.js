c2fadea2c2c6778d8ae68a279581aa28a35c66a1
Geanderson Esteves <14096071+gesteves91@users.noreply.github.com>
2022-08-12 22:07:51 -0400

    react project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
+import Container from "react-bootstrap/Container";
+import Header from "./components/Header";
+
 export default function App() {
-  const post = {
-    id: 1,
-    text: 'Hello World',
-    timestamp: 'a minute ago',
-    author: {
-      username: 'susan',
+  const posts = [
+    {
+      id: 1,
+      text: "Hello World",
+      timestamp: "a minute ago",
+      author: {
+        username: "susan",
+      },
     },
-  }
+    {
+      id: 2,
+      text: "Goodbye World",
+      timestamp: "an hour ago",
+      author: {
+        username: "john",
+      },
+    },
+  ];
 
   return (
-    <>
-      <h1>Microblog</h1>
-      <p>
-        <b>{post.author.username}</b> &mdash; {post.timestamp}
-        <br />
-        {post.text}
-      </p>
-    </>
+    <Container fluid className="App">
+      <Header />
+      <Container>
+        {posts.length === 0 ? (
+          <p>There are no blog posts.</p>
+        ) : (
+          posts.map((post) => {
+            return (
+              <p key={post.id}>
+                <b>{post.author.username}</b> &mdash; {post.timestamp}
+                <br />
+                {post.text}
+              </p>
+            );
+          })
+        )}
+      </Container>
+    </Container>
   );
 }
