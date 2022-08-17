e3723500ee8a2970b5dcd4cc1befe88c0b3dd6e8
Geanderson Esteves <14096071+gesteves91@users.noreply.github.com>
2022-08-16 20:46:52 -0400

    update the page

--- src/App.js
+++ src/App.js
@@ -1,51 +1,15 @@
 import Container from "react-bootstrap/Container";
-import Stack from "react-bootstrap/Stack";
 import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
+import Body from "./components/Body";
+import Posts from "./components/Posts";
 
 export default function App() {
-  const posts = [
-    {
-      id: 1,
-      text: "Hello World",
-      timestamp: "a minute ago",
-      author: {
-        username: "susan",
-      },
-    },
-    {
-      id: 2,
-      text: "Goodbye World",
-      timestamp: "an hour ago",
-      author: {
-        username: "john",
-      },
-    },
-  ];
-
   return (
     <Container fluid className="App">
       <Header />
-      <Container>
-        <Stack direction="horizontal">
-          <Sidebar />
-          <Container>
-            {posts.length === 0 ? (
-              <p>There are no blog posts.</p>
-            ) : (
-              posts.map((post) => {
-                return (
-                  <p key={post.id}>
-                    <b>{post.author.username}</b> &mdash; {post.timestamp}
-                    <br />
-                    {post.text}
-                  </p>
-                );
-              })
-            )}
-          </Container>
-        </Stack>
-      </Container>
+      <Body sidebar>
+        <Posts />
+      </Body>
     </Container>
   );
 }
